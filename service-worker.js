333ad4b50260a9fb4fccf7cdf6a3f5618b0b2c12
Ксения Лященко <ksenialasenko@MacBook-Pro-Ksenia.local>
2020-03-21 00:51:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/train-diploma/precache-manifest.0cbc1c833e673698a05ea2c9a362335b.js"
+  "/train-diploma/precache-manifest.fc3998096c9912a0146506fe50493501.js"
 );
 
 workbox.clientsClaim();
